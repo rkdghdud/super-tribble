d8b598cef6da07b3165cd39b1963d2ab3f82b131
rkdghdud <71686637+rkdghdud@users.noreply.github.com>
2021-05-27 18:53:43 +0900

    Update python_Tree.py

--- python_Tree.py
+++ python_Tree.py
@@ -3,4 +3,28 @@ class Node:
     self.value = value
     self.left = None
     self.right = None
+
+class NodeMgmt:
+  def __init__(self, head):
+    self.head = head
     
+  def insert(self, value):
+    self.current_node = self.head
+    while True:
+      if value < self.current_node.value:
+        if self.current_node.left != None:
+          self.current_node = self.current_node.left
+        else:
+          self.current_node.left = Node(value)
+          break
+      else:
+        if self.current_node.right != None:
+          self.current_node = self.current_node.right
+        else:
+          self.current_node.right = Node(value)
+          break
+          
+  def search(self, value):
+    self.current_node = self.head
+    
+          
