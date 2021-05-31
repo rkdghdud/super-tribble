25f835a1bed427ec923c8c37a6a271e94f214f59
rkdghdud <71686637+rkdghdud@users.noreply.github.com>
2021-05-31 16:34:51 +0900

    Update python_Tree.py
    implement search, delete function

--- python_Tree.py
+++ python_Tree.py
@@ -26,5 +26,41 @@ class NodeMgmt:
           
   def search(self, value):
     self.current_node = self.head
+    while self.current_node:
+      if self.current_node.value == value:
+        return True
+      elif self.current_node.value < value:
+        self.current_node = self.current_node.right
+      else:
+        self.current_node = self.current_node.left
+      return False
     
-          
+  def delete(self, value):
+    searched = False
+    self.current_node = self.head
+    self.parent = self.head
+    
+    while self.current_node;
+      if self.current_node.value == value:
+        searched = True
+        break
+      elif self.current_node.value < value:
+        self.parent = self.current_node
+        self.current_node = self.current_node.right
+      else:
+        self.parent = self.current_node
+        self.current_node = self.current_node.left
+        
+    if searched == False:
+      return False
+    
+    if self.current_node.left == None and self.current_node.right == None:
+      if self.current_node.value < value:
+        self.parent.right = None
+      else:
+        self.parent.left = None
+      del self.current_node
+      
+    
+    
+    
