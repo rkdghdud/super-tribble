a0f49973746bae76945d5b441edc8b7a46da8700
rkdghdud <71686637+rkdghdud@users.noreply.github.com>
2021-06-01 16:33:37 +0900

    Update python_Tree.py

--- python_Tree.py
+++ python_Tree.py
@@ -54,13 +54,25 @@ class NodeMgmt:
     if searched == False:
       return False
     
+		#삭제 목표 노드가 리프 노드일때
     if self.current_node.left == None and self.current_node.right == None:
-      if self.current_node.value < value:
+      if self.parent.value < value:
         self.parent.right = None
       else:
         self.parent.left = None
       del self.current_node
-      
-    
-    
-    
+    	return True
+		
+		#삭제 목표 노드에 자식노드가 한개 존재할 때
+    elif self.current_node.left == None and self.current_node.right != None:
+			if value < self.parent.value:
+				self.parent.left = self.currnet_node.right
+			else:
+				self.parent.right = self.current_node.right
+		elif self.current_node.right == None and self.current_node.left != None:
+			if value < self.parent.value:
+				self.parent.left = self.current_node.left
+			else:
+				self.parent.right = self.current_node.left
+			
+					
