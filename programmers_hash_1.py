8e18c6bb596cd6dcc9b01b78d85fddbd72be2582
rkdghdud <71686637+rkdghdud@users.noreply.github.com>
2021-06-02 14:00:36 +0900

    Create programmers_hash_1.py

---
+++ programmers_hash_1.py
@@ -0,0 +1,18 @@
+#https://programmers.co.kr/learn/courses/30/lessons/42576
+
+def solution(participant, completion):
+    completion.sort()
+    participant.sort()
+    
+    for i in completion:
+        participant.remove(i)
+
+    return participant[0]
+  
+  #테스트 케이스에 대한 값은 정상적으로 풀렸지만
+  #효율성 측면에서 한개만 통과되는 좋지않은 효율을 보였다.
+  #2021.06.02
+  #채점 결과
+  #정확성: 50.0
+  #효율성: 10.0
+  #합계: 60.0 / 100.0
