77bcfb2bb01130d6cb8f498e09745da3ed275126
rkdghdud <71686637+rkdghdud@users.noreply.github.com>
2021-07-04 14:50:12 +0900

    Update baekjoon_14916.py

--- baekjoon_14916.py
+++ baekjoon_14916.py
@@ -15,3 +15,13 @@ else:
     coin = ((n//5)-1) + ((n%5+5)//2)
     print(coin)
     
+=============================================
+n = int(input())
+coin = [50001, 50001, 1, 50001, 2, 1]
+
+for i in range(6,n+1):
+    coin.append(min(coin[i - 2] + 1, coin[i - 5] + 1))
+if coin[n]>50000:
+    print(-1)
+else:
+    print(coin[n])
